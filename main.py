ac237688ac2cdc9d8db6be232270abbd9ca82a84
MAXKIRITO <yxiao940@gmail.com>
2022-11-04 12:25:19 +0800

    A

--- main.py
+++ main.py
@@ -61,3 +61,7 @@ class how_to_print_a_hello_world:
     def call_os_to_print_hello_world(self):
 
         print(self.z)
+        
+    def use_some_magic_to_print_hello_world(slef):
+        print(''.join([chr(round(-(79*x**12)/31933440+(409*x**11)/3193344-(21697*x**10)/14515200-(67877*x**9)/1451520+(88163*x**8)/46080-(1029115*x**7)/32256+(4562068069*x **
+              6)/14515200-(2884823503*x**5)/1451520+(1191723787*x**4)/145152-(1580287133*x**3)/72576+(1391865319*x**2)/39600-(170471681*x)/5544+10940)) for x in range(1, 14)]))
--- v4.py
+++ v4.py
@@ -1,3 +1,3 @@
 from main import *
 
-how_to_print_a_hello_world().general_print_hello_world
+how_to_print_a_hello_world().use_some_magic_to_print_hello_world
